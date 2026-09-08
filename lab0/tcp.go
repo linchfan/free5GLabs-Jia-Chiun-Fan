9adad45385e4f164134ca266615918b7acd015b7
Jia-Chiun-Fan <fany122132@gmail.com>
2026-09-08 12:06:48 +0000

    Lab0: ver.3

--- lab0/tcp.go
+++ lab0/tcp.go
@@ -1,31 +1,35 @@
-package main
+package lab0
 
 import (
 	"fmt"
 	"net"
+	"strconv"
 )
 
-func TcpListener(IP string, port int, handler func(net.Conn) error) {
-	listener, err := net.Listen("tcp", IP+":"+string(port))
+type listenerInterface func(string, int, func(net.Conn) error)
+type handlerInterface func(net.Conn) error
+
+func TCPListener(IP string, port int, handler func(net.Conn) error) {
+	listener, err := net.Listen("tcp", net.JoinHostPort(IP, strconv.Itoa(port))) // Listen on the specified IP and port
 
 	if err != nil {
 		fmt.Println("Error listening", err.Error())
-		return // terminate
+		return // Terminate
 	}
 
-	fmt.Println("TCP is listening on ", IP, ":", string(port))
+	fmt.Println("TCP is listening on ", net.JoinHostPort(IP, strconv.Itoa(port)))
 
 	for {
-		conn, err := listener.Accept()
+		conn, err := listener.Accept() // Accept incoming connections
 		if err != nil {
 			fmt.Println("Error accepting", err.Error())
 			return
 		}
 
-		fmt.Println("new client accepted: ", IP, ":", string(port))
+		fmt.Println("new client accepted: ", net.JoinHostPort(IP, strconv.Itoa(port)))
 
 		go func() {
-			err := handler(conn)
+			err := handler(conn) // Handle the connection using the provided handler function
 			if err != nil {
 				fmt.Println("Error handling connection", err.Error())
 				return
@@ -34,12 +38,14 @@ func TcpListener(IP string, port int, handler func(net.Conn) error) {
 	}
 }
 
-func TcpHandler(conn net.Conn) error {
+func TCPHandler(conn net.Conn) error {
+	defer conn.Close()
+
 	remoteAddr := conn.RemoteAddr() // Client's remote address
 	tcpAddr, ok := remoteAddr.(*net.TCPAddr)
 
 	if !ok {
-		fmt.Println("Error getting remote address", err.Error())
+		fmt.Println("Error getting remote address")
 		err := fmt.Errorf("failed to get TCP remote address")
 		return err
 	}
@@ -47,4 +53,30 @@ func TcpHandler(conn net.Conn) error {
 	clientAddr := tcpAddr.IP.String() // Client's remote IP address
 	clientPort := tcpAddr.Port        // Client's remote port
 	fmt.Println("Handle Request from [", clientAddr, ":", clientPort, "]")
+
+	buf := make([]byte, 1024) // Prepare a buffer to read data from the connection
+
+	for {
+		n, err := conn.Read(buf) // Read data from the connection
+		if err != nil {          // EOF or other error
+			fmt.Println("Error reading", err.Error())
+			return err
+		}
+
+		if n > 0 { // Correctly read data from the connection
+			_, err = conn.Write(buf[:n]) // Echo back the received data to the client
+			if err != nil {
+				fmt.Println("Error writing", err.Error())
+				return err
+			}
+		} else if n == 0 { // EOF
+			fmt.Println("Client [", net.JoinHostPort(clientAddr, strconv.Itoa(clientPort)), "Error: EOF")
+			return nil
+		} else { // Error reading from the connection
+			fmt.Println("Error reading")
+			return nil
+		}
+	}
+
+	return nil
 }
