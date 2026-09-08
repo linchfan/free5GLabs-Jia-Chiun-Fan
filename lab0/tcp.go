cc6bb89e567190d6b1b04553e94e6f234e20ed54
Jia-Chiun-Fan <fany122132@gmail.com>
2026-09-08 11:36:53 +0000

    Lab0: ver.2

--- lab0/tcp.go
+++ lab0/tcp.go
@@ -13,6 +13,8 @@ func TcpListener(IP string, port int, handler func(net.Conn) error) {
 		return // terminate
 	}
 
+	fmt.Println("TCP is listening on ", IP, ":", string(port))
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -20,13 +22,29 @@ func TcpListener(IP string, port int, handler func(net.Conn) error) {
 			return
 		}
 
-		err = handler(conn)
-		if err != nil {
-			fmt.Println("Error handling connection", err.Error())
-		}
+		fmt.Println("new client accepted: ", IP, ":", string(port))
+
+		go func() {
+			err := handler(conn)
+			if err != nil {
+				fmt.Println("Error handling connection", err.Error())
+				return
+			}
+		}()
 	}
 }
 
-func TcpHandler() error {
+func TcpHandler(conn net.Conn) error {
+	remoteAddr := conn.RemoteAddr() // Client's remote address
+	tcpAddr, ok := remoteAddr.(*net.TCPAddr)
 
+	if !ok {
+		fmt.Println("Error getting remote address", err.Error())
+		err := fmt.Errorf("failed to get TCP remote address")
+		return err
+	}
+
+	clientAddr := tcpAddr.IP.String() // Client's remote IP address
+	clientPort := tcpAddr.Port        // Client's remote port
+	fmt.Println("Handle Request from [", clientAddr, ":", clientPort, "]")
 }
